9c2526219624411c6d0e1de8be9e84aa85077bc6
Umar Khan <uk4495848@gmail.com>
2024-08-14 08:02:35 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './conponents/Card'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='bg-green-400 rounded-xl mb-4'>tailwind test</h1>
+    
+
+  <Card username="Umer farooq"  text="my name is umar farooq khan."/>
+</>
+  )
+}
+
+export default App
